91a5587ba31887f9766974e2cba0af1af69d3b60
Jeison Eslava <jeslava21>
2024-04-19 16:49:11 -0500

    AJUSTE SH

--- start.sh
+++ start.sh
@@ -6,13 +6,13 @@ DOCKER_PASSWORD="clave2198"
 DOCKER_REPO="prueba-weather-app"
 
 # Construir la imagen Docker
-docker build -t $DOCKER_REPO .
+docker build -t $DOCKER_REPO:$1 .
 
 # Iniciar sesión en Docker Hub
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
 
 # Subir la imagen al registro público de Docker Hub
-docker tag $DOCKER_REPO $DOCKER_USERNAME/$DOCKER_REPO
-docker push $DOCKER_USERNAME/$DOCKER_REPO
+docker tag $DOCKER_REPO:$1 $DOCKER_USERNAME/$DOCKER_REPO
+docker push $DOCKER_USERNAME/$DOCKER_REPO:$1
 
 
